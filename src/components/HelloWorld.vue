c431ce9d0101bfc36477eded7a860a6eb9db516f
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-12 19:35:00 -0400

    Add bootstrap, vuex, and router

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ msg }}<span class="badge badge-secondary">New</span></h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
