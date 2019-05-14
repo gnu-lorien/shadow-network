866d87320c97f53819af64a4d454716880af40cd
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-14 01:11:35 -0400

    Getting some basic Parse stuff going

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/resources">Resources</router-link>
     </div>
     <router-view/>
   </div>
@@ -18,3 +20,12 @@
   margin-top: 60px;
 }
 </style>
+
+<script>
+  /* global Parse */
+  Parse.serverURL = 'https://parseapi.back4app.com'; // This is your Server URL
+  Parse.initialize(
+          '5lGxsIwF81tR4LjrzxJsgpIdmDZaIvNWshmpXhNz', // This is your Application ID
+          'g8q6x9uvsept5Sjfz3hdiiP3mh5mgOoda2rZeP4I' // This is your Javascript key
+  );
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="text-center">
+        <form class="form-signin">
+            <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+            <label for="inputEmail" class="sr-only">Email address</label>
+            <input v-model="email" type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input  v-model="password" type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+            <button v-on:click="login" class="btn btn-lg btn-primary btn-block" type="button">Sign in</button>
+            <p class="mt-5 mb-3 text-muted">© 2017-2018</p>
+        </form>
+    </div>
+</template>
+
+<script>
+    /* global Parse */
+    export default {
+        name: "Login",
+        data: function () {
+            return {
+                email: "",
+                password: ""
+            }
+        },
+        methods: {
+            login() {
+                if (this.email.length === 0) {
+                    alert("Please enter an email");
+                    return;
+                }
+                if (this.password.length === 0) {
+                    alert("Please enter a password");
+                    return;
+                }
+
+                Parse.Cloud.run('hello')
+                    .then((response) => {
+                        alert("Nothing?");
+                        alert(JSON.stringify(response));
+                    })
+                    .catch(function (e) {
+                        alert("Error calling hello " + e.message);
+                    });
+                alert("What?");
+                Parse.User.logIn(this.email, this.password)
+                    .then(() => {
+                        // Used an arrow function here because I
+                        // want to access 'this' which is overridden in
+                        // a conventional function
+                        this.$router.replace("/");
+                    })
+                    .catch(function (e) {
+                        alert("Error logging in! " + e.message);
+                    });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
