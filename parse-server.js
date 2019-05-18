eab951ceff20faa9013b0695cc27713a99cb6103
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-17 22:19:49 -0400

    Add Parse Dashboard

--- parse-server.js
+++ parse-server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     http = require('http'),
-    ParseServer = require('parse-server').ParseServer;
+    ParseServer = require('parse-server').ParseServer,
+    ParseDashboard = require('parse-dashboard');
 
 var app = express();
 const port = process.env.PORT || 1337;
@@ -16,7 +17,21 @@ var settings = {
     "serverURL": "http://0.0.0.0:" + port + "/parse/1"
 };
 
+var dashboardSettings = {
+    "apps": [
+        {
+            "serverURL": "http://localhost:" + port + "/parse/1",
+            "appId": process.env.APPLICATION_ID || "APPLICATION_ID",
+            "masterKey": process.env.MASTER_KEY || "MASTER_KEY",
+            "appName": process.env.APPLICATION_NAME || "Shadow Network"
+        }
+    ]
+}
+
 var api = new ParseServer(settings);
 app.use('/parse/1', api);
+
+var dashboard = new ParseDashboard(dashboardSettings);
+app.use('/dashboard', dashboard);
 
 http.createServer(app).listen(port, '0.0.0.0');
