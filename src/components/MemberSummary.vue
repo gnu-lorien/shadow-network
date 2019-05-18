ad4c5a00659239dbc1a292f10c6d83ba62687b55
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-18 00:53:43 -0400

    Moving to a component having the members
    This needs to move to Vuex basically the moment I prove it out to avoid so many extra queries.

---
+++ src/components/MemberSummary.vue
@@ -0,0 +1,35 @@
+<template>
+    <p>A member with {{member.id}} and {{member.attributes.name}}</p>
+</template>
+
+<script>
+    /* global Parse */
+    let Member = Parse.Object.extend("Member");
+    export default {
+        name: "MemberSummary",
+        data: function () {
+            return {
+                member: {}
+            }
+        },
+        mounted: function() {
+            this.fetch();
+        },
+        methods: {
+            fetch() {
+                const q = new Parse.Query(Member);
+                q.get(this.$props.memberId)
+                    .then((member) => {
+                        this.member = member;
+                    });
+            }
+        },
+        props: [
+            'memberId'
+        ]
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,8 +1,6 @@
 <template>
     <div class="container">
-        <div class="row" v-for="member in members">
-            <p>A member with {{member.id}} and {{member.get('name')}}</p>
-        </div>
+        <member-summary class="row" v-for="id in members" :key="id" :memberId="id" />
         <div class="row">
             <button v-on:click="add">Add</button>
             <button v-on:click="changeaname">Change A Name</button>
@@ -13,6 +11,8 @@
 <script>
     /* global Parse */
     let Member = Parse.Object.extend("Member");
+    import Vue from 'vue';
+    import MemberSummary from '@/components/MemberSummary.vue'
     export default {
         name: "Members.vue",
         data: function () {
@@ -32,7 +32,7 @@
                 });
                 q.find()
                     .then((members) => {
-                        this.members = members;
+                        this.members = members.map(member => member.id);
                     });
             },
             add() {
@@ -47,7 +47,6 @@
                 member.set('street_name', 'Unknown street name');
                 member.save()
                     .then((member) => {
-                        alert("New member created with id " + member.id);
                         this.members = [member].concat(this.members);
                     })
                     .catch(function(e) {
@@ -57,7 +56,12 @@
             changeaname() {
                 const member = this.members[0];
                 member.set('name', 'Something else');
+                //member.attributes.name = 'Something else';
+                Vue.set(member, 'attributes.name', 'Something else');
             }
+        },
+        components: {
+            MemberSummary
         }
     }
 </script>
