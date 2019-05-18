7aa0d26a2489f0209cf9f0c03544e095f639fbcf
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-18 01:11:41 -0400

    Component properly changing the name

--- src/components/MemberSummary.vue
+++ src/components/MemberSummary.vue
@@ -1,15 +1,27 @@
 <template>
-    <p>A member with {{member.id}} and {{member.attributes.name}}</p>
+    <div>
+        <p>A member with {{member.id}} and {{member.attributes.name}}</p>
+        <p><button v-on:click="changename">Change A Name</button></p>
+    </div>
 </template>
 
 <script>
     /* global Parse */
     let Member = Parse.Object.extend("Member");
+    import Vue from 'vue';
     export default {
         name: "MemberSummary",
+        props: [
+            'memberId'
+        ],
         data: function () {
             return {
-                member: {}
+                member: {
+                    id: "",
+                    attributes: {
+                        name: ""
+                    }
+                }
             }
         },
         mounted: function() {
@@ -20,13 +32,17 @@
                 const q = new Parse.Query(Member);
                 q.get(this.$props.memberId)
                     .then((member) => {
-                        this.member = member;
+                        this.setupMember(member);
                     });
+            },
+            setupMember(member) {
+                this.member = member;
+            },
+            changename() {
+                this.member.set('name', 'A New Name');
+                Vue.set(this.member, 'attributes.name', 'A New Name');
             }
-        },
-        props: [
-            'memberId'
-        ]
+        }
     }
 </script>
 
