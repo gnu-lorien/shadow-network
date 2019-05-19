c8c2fefc44b3ef1d3a1d9356d8a3d3621611e63f
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-19 10:54:11 -0400

    Reformat router.js

--- src/router.js
+++ src/router.js
@@ -8,36 +8,37 @@ import Resources from './views/Resources.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/members',
-      name: 'members',
-      component: Members
-    },
-    {
-      path: '/resources',
-      name: 'resources',
-      component: Resources
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        },
+        {
+            path: '/members',
+            name: 'members',
+            component: Members
+        },
+        {},
+        {
+            path: '/resources',
+            name: 'resources',
+            component: Resources
+        }
+    ]
 })
