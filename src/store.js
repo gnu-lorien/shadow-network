8db82fcf0a42f8798b2ad2c988d3c6fbf3111909
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-19 12:34:10 -0400

    Failing to move to vuex modules

--- src/store.js
+++ src/store.js
@@ -2,8 +2,46 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Member from '@/models/member.js'
+import Resource from '@/models/resource.js';
 
 Vue.use(Vuex)
+
+let MemberModule = {
+    state: {
+        member: {
+            id: "",
+            name: ""
+        }
+    },
+    mutations: {
+        setCurrentMember(state, member) {
+            state.member.id = member.id;
+            for (const key in member.attributes) {
+                Vue.set(state.member, `${key}`, member.get(key));
+            }
+        }
+    },
+    actions: {
+        loadOrUseMember(context, memberId) {
+            if (context.state.member.id === memberId) {
+                return Promise.resolve();
+            }
+            const q = new Parse.Query(Member);
+            return q.get(memberId)
+                .then((member) => {
+                    context.commit('setCurrentMember', member);
+                });
+        }
+    }
+}
+
+/*
+export default new Vuex.Store({
+    modules: [
+        MemberModule
+    ]
+})
+ */
 
 export default new Vuex.Store({
     state: {
