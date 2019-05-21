ba725b99af27a43c6f228858bdfd4cf4fe77ed8d
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-20 22:02:35 -0400

    Use Bootswatch Cyborg theme

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+require('./assets/css/bootstrap.min.css');
 
 Vue.config.productionTip = false
 
