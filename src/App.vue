10e3d824932385bb06d1297b07cf8dc7a9178f8d
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-20 22:26:35 -0400

    Use bootstrape-vue components

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav" class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Shadow Network</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarColor02">
-        <ul class="navbar-nav mr-auto">
-          <router-link tag="li" class="nav-item" exact-active-class="active" to="/">
-            <a class="nav-link">Home</a>
-          </router-link>
-          <router-link tag="li" class="nav-item" exact-active-class="active" to="/about">
-            <a class="nav-link">About</a>
-          </router-link>
-          <router-link tag="li" class="nav-item" exact-active-class="active" to="/login">
-            <a class="nav-link">Login</a>
-          </router-link>
-          <router-link tag="li" class="nav-item" exact-active-class="active" to="/members">
-            <a class="nav-link">Members</a>
-          </router-link>
-        </ul>
-          <!--
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search">
-          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-        </form>
-        -->
-      </div>
+    <div id="app">
+        <b-navbar id="nav" toggleable="lg" type="dark">
+            <b-navbar-brand href="#">Shadow Network</b-navbar-brand>
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item to="/">Home</b-nav-item>
+                    <b-nav-item to="/about">About</b-nav-item>
+                    <b-nav-item to="/login">Login</b-nav-item>
+                    <b-nav-item to="/members">Members</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+        <header-view/>
+        <router-view/>
     </div>
-    <header-view/>
-    <router-view/>
-  </div>
 </template>
 
 <script>
-  /* global Parse */
-  Parse.serverURL = 'https://shadownetwork.herokuapp.com/parse/1'; // This is your Server URL
-  Parse.initialize(
-          'APPLICATION_ID', // This is your Application ID
-          'g8q6x9uvsept5Sjfz3hdiiP3mh5mgOoda2rZeP4I' // This is your Javascript key
-  );
+    /* global Parse */
+    Parse.serverURL = 'https://shadownetwork.herokuapp.com/parse/1'; // This is your Server URL
+    Parse.initialize(
+        'APPLICATION_ID', // This is your Application ID
+        'g8q6x9uvsept5Sjfz3hdiiP3mh5mgOoda2rZeP4I' // This is your Javascript key
+    );
 
-  import HeaderView from './views/HeaderView.vue'
+    import HeaderView from './views/HeaderView.vue'
 
-  export default {
-    name: "App",
-    components: {
-      HeaderView
+    export default {
+        name: "App",
+        components: {
+            HeaderView
+        }
     }
-  }
 </script>
