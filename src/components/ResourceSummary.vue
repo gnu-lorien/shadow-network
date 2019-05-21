fbd8011e03a222110d32aaf394a5100aadbfd345
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-20 23:14:21 -0400

    Better resource summary

--- src/components/ResourceSummary.vue
+++ src/components/ResourceSummary.vue
@@ -1,24 +1,35 @@
 <template>
     <div>
-        <p>A resource with {{resource.id}} and {{resource.name}}</p>
-        <p>
-            <button v-on:click="edit">Edit</button>
-        </p>
-        <p>
-            <button v-on:click="remove">Delete</button>
-        </p>
+        <b-card>
+            <b-card-title>{{resource.name}}</b-card-title>
+            <b-card-sub-title>{{resource.id}}</b-card-sub-title>
+            <b-list-group>
+                <component-summary v-for="id in componentIds" :key="id" :componentId="id" :memberId="memberId" :resourceId="resourceId" />
+            </b-list-group>
+            <b-card-footer>
+                <button v-on:click="edit">Edit</button>
+                <button v-on:click="remove">Delete</button>
+            </b-card-footer>
+        </b-card>
     </div>
 </template>
 
 <script>
+    import ComponentSummary from '@/components/ComponentSummary.vue';
     export default {
         name: "ResourceSummary",
         props: [
             'resourceId'
         ],
+        components: {
+            ComponentSummary
+        },
         computed: {
             resource() {
                 return this.$store.state.resources.resources[this.$props.resourceId] || { id: "" , name: ""};
+            },
+            componentIds() {
+                return this.resource.componentIds;
             }
         },
         mounted: function () {
@@ -26,7 +37,13 @@
         },
         methods: {
             fetch() {
-                this.$store.dispatch('loadOrUseResource', this.$props.resourceId);
+                this.$store.dispatch('loadOrUseResource', this.$props.resourceId)
+                    .then(() => {
+                        return this.$store.dispatch('loadOrUseResourceComponents', this.$props.resourceId);
+                    })
+                    .catch((e) => {
+                        alert("Failed to load resource or components " + e.message);
+                    });
             },
             edit() {
                 this.$router.push({
