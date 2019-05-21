29d11593ae4743937648891b8d70f5cb5fa7ef2e
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-20 22:02:58 -0400

    Reformatting navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/members">Members</router-link>
+    <div id="nav" class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Shadow Network</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor02">
+        <ul class="navbar-nav mr-auto">
+          <router-link tag="li" class="nav-item" exact-active-class="active" to="/">
+            <a class="nav-link">Home</a>
+          </router-link>
+          <router-link tag="li" class="nav-item" exact-active-class="active" to="/about">
+            <a class="nav-link">About</a>
+          </router-link>
+          <router-link tag="li" class="nav-item" exact-active-class="active" to="/login">
+            <a class="nav-link">Login</a>
+          </router-link>
+          <router-link tag="li" class="nav-item" exact-active-class="active" to="/members">
+            <a class="nav-link">Members</a>
+          </router-link>
+        </ul>
+          <!--
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search">
+          <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        -->
+      </div>
     </div>
     <header-view/>
     <router-view/>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
 
 <script>
   /* global Parse */
