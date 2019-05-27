8d4234097f3b0031ba98d375c4d2519bcb788cd0
Andrew Sayman <andrew.sayman@gmail.com>
2019-05-26 20:12:46 -0400

    Can sign out from the navbar

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@
                     <b-nav-item-dropdown right>
                         <!-- Using 'button-content' slot -->
                         <template slot="button-content"><em>User</em></template>
-                        <b-dropdown-item href="#">Profile</b-dropdown-item>
-                        <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                        <!-- <b-dropdown-item href="#">Profile</b-dropdown-item> -->
+                        <b-dropdown-item @click.prevent="signout">Sign Out</b-dropdown-item>
                     </b-nav-item-dropdown>
                 </b-navbar-nav>
             </b-collapse>
@@ -38,6 +38,9 @@
                 } else {
                     this.$router.push('/');
                 }
+            },
+            async signout() {
+                await Parse.User.logOut();
             }
         }
     }
