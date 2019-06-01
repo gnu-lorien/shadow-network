2bb1736a4cb8ac931ae2973e360295eecbff81ca
Andrew Sayman <andrew.sayman@gmail.com>
2019-06-01 15:50:18 -0400

    Put a border around the member icon

--- src/components/MemberSummary.vue
+++ src/components/MemberSummary.vue
@@ -1,7 +1,7 @@
 <template>
     <b-card class="w-100 h-100 p-0 m-0" body-class="p-1">
         <b-card-header class="p-0">
-            <img :src="portraitthumb_64"/>
+            <img class="border-info" style="border-width: 1px; border-style: solid" :src="portraitthumb_64" v-if="portraitthumb_64"/>
             <button class="ml-1" v-on:click="$emit('member-selected', member.id)">Select</button>
         </b-card-header>
         <b-card-title>{{member.street_name}}</b-card-title>
